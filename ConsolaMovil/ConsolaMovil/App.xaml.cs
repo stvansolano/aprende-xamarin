421ba84b61b4a7e603cb4c78567d2bd14d0b094f
stvansolano <stvansolano@outlook.com>
2019-02-18 15:24:55 -0600

    Consola parte 1

--- ConsolaMovil/ConsolaMovil/App.xaml.cs
+++ ConsolaMovil/ConsolaMovil/App.xaml.cs
@@ -12,6 +12,27 @@ namespace ConsolaMovil
             InitializeComponent();
 
             MainPage = new MainPage();
+
+            // declaracion de variables
+            string miNombre = "John";
+
+            string nombreCompleto = "Esteban" + " " + "Solano";
+            string concatenacion = "Esteban" + "Solano" + 1 + 1;
+
+            int miEntero = (1 + 1) / 10; //   2 / 10 = 0.2
+            float miFlotante = (float)(1 + 1) / 10;
+
+            // Palabra reservada VAR
+            var salario = 123.45f;
+
+            bool miBooleano = true;
+
+            Console.WriteLine("Valor entero:" + miEntero);
+            Console.WriteLine("Nombre: " + nombreCompleto);
+            Console.WriteLine("Concatenacion: " + concatenacion);
+            Console.WriteLine("Division: " + miFlotante);
+            Console.WriteLine("El salario es: " + salario);
+            Console.WriteLine(miBooleano);
         }
 
         protected override void OnStart()
