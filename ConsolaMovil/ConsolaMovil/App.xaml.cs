7923819e4583797a029922720edfff9abb5a61f1
stvansolano <stvansolano@outlook.com>
2019-02-18 18:24:27 -0600

    Avance en consola movil

--- ConsolaMovil/ConsolaMovil/App.xaml.cs
+++ ConsolaMovil/ConsolaMovil/App.xaml.cs
@@ -1,53 +1,19 @@
 ﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace ConsolaMovil
 {
-    public partial class App : Application
+    public partial class App
     {
+        // Constructor de la clase
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // Asignacion de la propiedad "MainPage"
+            var pagina = new Xamarin.Forms.NavigationPage(new MainPage());
+            pagina.CurrentPage.Title = "Consola movil";
 
-            // declaracion de variables
-            string miNombre = "John";
-
-            string nombreCompleto = "Esteban" + " " + "Solano";
-            string concatenacion = "Esteban" + "Solano" + 1 + 1;
-
-            int miEntero = (1 + 1) / 10; //   2 / 10 = 0.2
-            float miFlotante = (float)(1 + 1) / 10;
-
-            // Palabra reservada VAR
-            var salario = 123.45f;
-
-            bool miBooleano = true;
-
-            Console.WriteLine("Valor entero:" + miEntero);
-            Console.WriteLine("Nombre: " + nombreCompleto);
-            Console.WriteLine("Concatenacion: " + concatenacion);
-            Console.WriteLine("Division: " + miFlotante);
-            Console.WriteLine("El salario es: " + salario);
-            Console.WriteLine(miBooleano);
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+            MainPage = pagina;
         }
     }
 }
--- ConsolaMovil/ConsolaMovil/MainPage.xaml.cs
+++ ConsolaMovil/ConsolaMovil/MainPage.xaml.cs
@@ -12,6 +12,37 @@ namespace ConsolaMovil
         public MainPage()
         {
             InitializeComponent();
+
+            // Codigo de la consola
+            string miNombre = "John";
+
+            string nombreCompleto = "Esteban" + " " + "Solano";
+            string concatenacion = "Esteban" + "Solano" + 1 + 1;
+
+            int miEntero = (1 + 1) / 10; //   2 / 10 = 0.2
+            float miFlotante = (float)(1 + 1) / 10;
+
+            // Palabra reservada VAR
+            var salario = 123.45f;
+
+            bool miBooleano = true;
+
+            string salida = "";
+
+            salida += "Valor entero:" + miEntero;
+            salida += "Nombre: " + nombreCompleto;
+            salida += "Concatenacion: " + concatenacion;
+            salida += "Division: " + miFlotante;
+            salida += "El salario es: " + salario;
+            salida += miBooleano;
+
+            // Tarea: Presentar los Label en un StackLayout
+            new Label { Text = "Valor entero:" + miEntero };
+            new Label { Text = "Nombre:" + nombreCompleto};
+            // ....
+            new Label { Text = miBooleano.ToString() };
+
+            LabelSalida.Text = salida;
         }
     }
 }
