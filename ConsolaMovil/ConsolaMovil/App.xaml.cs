e0f09233a5513525081080914fbd3a6fca8e73f5
stvansolano <stvansolano@outlook.com>
2019-02-21 18:30:32 -0600

    Sesion 4

--- ConsolaMovil/ConsolaMovil/App.xaml.cs
+++ ConsolaMovil/ConsolaMovil/App.xaml.cs
@@ -15,5 +15,43 @@ namespace ConsolaMovil
 
             MainPage = pagina;
         }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            MostrarSalida("La aplicacion se ha iniciado");
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+
+            MostrarSalida("La aplicacion se ha interrumpido");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            MostrarSalida("La aplicacion se ha reanudado");
+        }
+
+        private void MostrarSalida(string mensaje)
+        {
+            // Nivel 1: Propiedad "MainPage"
+            var main = (Xamarin.Forms.NavigationPage)MainPage;
+
+            // Nivel 2: Propiedad "RootPage"
+            var root = main.RootPage;
+
+            // Nivel 3: Casteo al tipo "MainPage"
+            MainPage paginaActual = (MainPage)root;
+
+            paginaActual.MostrarMensaje(mensaje);
+
+            Console.WriteLine(mensaje);
+        }
+
     }
 }
